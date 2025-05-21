d42a5c0d63c4623d48722ca049c93329d38d2ddd
Piyush <166606245+Piyush232005@users.noreply.github.com>
2025-05-21 22:02:09 +0530

    Use of Context Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from "react";
 import Create from "./Component/Create";
 import Read from "./Component/Read";
 import "./index.css";
 
 const App = () => {
-    const [todos, settodos] = useState([
-        { id: 1, title: "Kamm krle bhai", isCompleted: false },
-    ]);
+
 
     return (
         <div className="text-white flex w-screen h-screen bg-gray-700 p-10">
-            <Create todos={todos} settodos={settodos} />
-            <Read todos={todos} settodos={settodos} />
+            <Create />
+            <Read />
         </div>
     );
 };
--- src/Component/Read.jsx
+++ src/Component/Read.jsx
@@ -1,9 +1,9 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import { toast } from "react-toastify";
+import { todocontext } from "../Wrapper";
 
-const Read = (props) => {
-    const todos = props.todos;
-    const settodos = props.settodos;
+const Read = () => {
+    const [todos, settodos] =  useContext(todocontext);
 
 
     const DeleteHandler = (id)=> {
